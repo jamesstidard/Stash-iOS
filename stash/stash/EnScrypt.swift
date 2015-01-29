dc5916718321bcafcb4510fcad0935e3adbd077d
James Stidard <jamesstidard@James-MacBook-Pro.local>
2015-01-29 19:07:54 +0000

    xor operator for nsdata/mutabledata

--- stash/stash/EnScrypt.swift
+++ stash/stash/EnScrypt.swift
@@ -15,7 +15,6 @@ class EnScrypt {
     class func salsa208Sha256(password: NSData?, var salt: NSData?, N: UInt64, r: UInt32, p: UInt32, i: Int) -> NSData?
     {
         var finalOut: NSMutableData?
-        var finalOutPtr: UnsafeMutablePointer<UInt8>?
         
         for x in 1...i {
             
@@ -26,19 +25,14 @@ class EnScrypt {
                 
                 // if first cycle then store initial out into final and get pointer
                 if x == 1 {
-                    finalOut    = out.mutableCopy() as? NSMutableData
-                    finalOutPtr = UnsafeMutablePointer<UInt8>(finalOut!.bytes)
+                    finalOut = out.mutableCopy() as? NSMutableData
                 }
-                    // else, XOR the out with the running finalOut and store back into out
+                // else, XOR the out with the running finalOut and store back into out
                 else {
-                    var outPtr = UnsafeMutablePointer<UInt8>(out.bytes)
-                    for byte in 0..<out.length {
-                        finalOutPtr![byte] = finalOutPtr![byte] ^ outPtr[byte]
-                    }
+                    finalOut = finalOut! ^ out
                 }
             }
-            else
-            {
+            else {
                 return nil
             }
         }
