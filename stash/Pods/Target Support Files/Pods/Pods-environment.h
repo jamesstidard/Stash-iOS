32ae671cbc5ef6643d2692e505cd836c43c2bfcc
James Stidard <jamesstidard@James-MacBook-Pro.local>
2015-02-06 22:07:52 +0000

    added OpenSSL pod

--- stash/Pods/Target Support Files/Pods/Pods-environment.h
+++ stash/Pods/Target Support Files/Pods/Pods-environment.h
@@ -6,6 +6,12 @@
 // project.
 
 
+// OpenSSL
+#define COCOAPODS_POD_AVAILABLE_OpenSSL
+#define COCOAPODS_VERSION_MAJOR_OpenSSL 1
+#define COCOAPODS_VERSION_MINOR_OpenSSL 0
+#define COCOAPODS_VERSION_PATCH_OpenSSL 200
+
 // libsodium
 #define COCOAPODS_POD_AVAILABLE_libsodium
 #define COCOAPODS_VERSION_MAJOR_libsodium 1
