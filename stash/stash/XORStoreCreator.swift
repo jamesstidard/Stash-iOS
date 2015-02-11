431b986e9f3ac3ecba8ed46efd0a2b3e9c20e292
James Stidard <jamesstidard@James-MacBook-Pro.local>
2015-02-11 11:32:04 +0000

    init password management for XORStore

--- stash/stash/XORStoreCreator.swift
+++ stash/stash/XORStoreCreator.swift
@@ -13,48 +13,25 @@ let XORStoreClassNameKey = "XORStore"
 extension XORStore {
     
     class func createXORStore(inout sensitiveData: NSData, password: NSData, context: NSManagedObjectContext) -> XORStore? {
-        let N = EnScryptDefaultNCost
-        let r = EnScryptDefaultRCost
-        let p = EnScryptDefaultParallelisation
-        let i = EnScryptDefaultIterations
         
-        let saltBytes         = 32
-        let verificationBytes = 16
+        var newStore: XORStore?
         
-        
-        // Generate a random salt and hash with the plaintext password
-        let salt = SodiumUtilities.randomBytes(saltBytes)
-        var key  = EnScrypt.salsa208Sha256(password, salt: salt, N: N, r: r, p: p, i: i)
-        // Quit if unable to generate key from hashing password
-        if key == nil || salt == nil {
-            return nil
-        }
-        
-        
-        // make sure the plaintext is the same length as the hashed password so they can be safely XORd
-        if key?.length != sensitiveData.length {
-            return nil
-        }
-        // Attach password by XORing
-        let ciphertext = sensitiveData ^ key!
-        // Store the first part of sequental hash on key for password verification
-        var verificationTag = Sha256.hash(key!)?.subdataWithRange(NSRange(location: 0, length: verificationBytes))
-        if verificationTag == nil {
-            return nil
-        }
-        
-        
-        // Create the new store and assign its properties
-        var newStore: XORStore!
-        
-        context.performBlockAndWait {
-            newStore = NSEntityDescription.insertNewObjectForEntityForName(XORStoreClassNameKey, inManagedObjectContext: context) as XORStore
+        if let var newKeyBundle = XORStore.makeKeyFromPassword(password) {
             
-            newStore.ciphertext         = ciphertext
-            newStore.scryptIterations   = i
-            newStore.scryptMemoryFactor = N
-            newStore.scryptSalt         = salt!
-            newStore.verificationTag    = verificationTag!
+            // make sure the plaintext is the same length as the hashed password so they can be safely XORd
+            if newKeyBundle.key.length == sensitiveData.length {
+                
+                // Create the new store and assign its properties
+                context.performBlockAndWait {
+                    newStore = NSEntityDescription.insertNewObjectForEntityForName(XORStoreClassNameKey, inManagedObjectContext: context) as? XORStore
+                    
+                    newStore?.ciphertext         = sensitiveData ^ newKeyBundle.key
+                    newStore?.scryptIterations   = newKeyBundle.i
+                    newStore?.scryptMemoryFactor = newKeyBundle.N
+                    newStore?.scryptSalt         = newKeyBundle.salt
+                    newStore?.verificationTag    = newKeyBundle.tag
+                }
+            }
         }
         
         return newStore
--- stash/stash/XORStorePasswordManagement.swift
+++ stash/stash/XORStorePasswordManagement.swift
@@ -12,41 +12,70 @@ extension XORStore {
     
     func decryptCipherTextWithPassword(password: NSData) -> NSData?
     {
+        if let key = keyFromPassword(password) {
+            if isValidKey(key) {
+                return self.ciphertext ^ key
+            }
+        }
+        return nil
+    }
+    
+    class func makeKeyFromPassword(password: NSData) -> (key: NSData, salt: NSData, N: UInt64, i: Int, tag: NSData)? {
+        let N = EnScryptDefaultNCost
+        let r = EnScryptDefaultRCost
+        let p = EnScryptDefaultParallelisation
+        let i = EnScryptDefaultIterations
+        
+        if let salt = SodiumUtilities.randomBytes(32) {
+            if let newKey = EnScrypt.salsa208Sha256(password, salt: salt, N: N, r: r, p: p, i: i) {
+                if let tag = XORStore.verificationTagFromKey(newKey) {
+                    return (newKey, salt, N, i, tag)
+                }
+            }
+        }
+        
+        return nil
+    }
+    
+    func keyFromPassword(password: NSData) -> NSData? {
         let i = self.scryptIterations
         let N = self.scryptMemoryFactor
         let r = Scrypt.rValueFrom(N)
         let p = EnScryptDefaultParallelisation
         
-        // TODO: Enable in swift v1.2
-//        if let
-//            i = self.scryptIterations,
-//            N = self.scryptMemoryFactor
-//            r = Scrypt.rValueFrom(N)
-//            p = EnScryptDefaultParallelisation {
-//            
-//        }
-        
         if r == nil {
             return nil
         }
         
-//        if let
-//            key = EnScrypt.salsa208Sha256(password, salt: self.scryptSalt, N: N, r: r!, p: p, i: i),
-//            tag = XORStore.verificationTagFromKey(key)
-//            where tag == self.verificationTag {
-//            
-//            
-//        }
-        
-        
-        
-        
-        return nil
+        return EnScrypt.salsa208Sha256(password, salt: self.scryptSalt, N: N, r: r!, p: p, i: i)
     }
     
     // change password
+    func changePassword(oldPassword: NSData, newPassword: NSData) -> Bool {
+        // try decrypt under old password
+        if let decryptedData = self.decryptCipherTextWithPassword(oldPassword) {
+            // create new key from password
+            if let newKeyBundle = XORStore.makeKeyFromPassword(newPassword) {
+                // encrypt and store new cipher data under new key and update properties of new key
+                self.ciphertext         = decryptedData ^ newKeyBundle.key
+                self.scryptIterations   = newKeyBundle.i
+                self.scryptMemoryFactor = newKeyBundle.N
+                self.scryptSalt         = newKeyBundle.salt
+                self.verificationTag    = newKeyBundle.tag
+                
+                return true
+            }
+        }
+        return false
+    }
     
-    // verify password
+    // verify key
+    func isValidKey(key: NSData) -> Bool {
+        if let tag = XORStore.verificationTagFromKey(key) {
+            return tag.isEqualToData(self.verificationTag)
+        }
+        return false
+    }
     
     // varification tag for key
     class func verificationTagFromKey(key: NSData) -> NSData? {
