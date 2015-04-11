2874ebd836b88005f5b06d95afbef531a62b348e
James Stidard <jamesstidard@James-MacBook-Pro.local>
2015-04-11 22:22:45 +0100

    camera QRCode scanner plus fixed scaling issue for iPhone 6 and 6 plus

--- stash/stash/QRScannerViewController.swift
+++ stash/stash/QRScannerViewController.swift
@@ -7,18 +7,107 @@
 //
 
 import UIKit
+import AVFoundation
 
-class QRScannerViewController: UIViewController {
-
-    override func viewDidLoad() {
+class QRScannerViewController: UIViewController,
+    AVCaptureMetadataOutputObjectsDelegate
+{
+    @IBOutlet weak var previewView: UIView!
+    
+    private var videoPreviewLayer: AVCaptureVideoPreviewLayer?
+    private var captureDevice:     AVCaptureDevice?
+    private var _captureSession:   AVCaptureSession?
+    private var captureSession:    AVCaptureSession {
+        if _captureSession == nil {
+            _captureSession = AVCaptureSession()
+        }
+        return _captureSession!
+    }
+    
+    
+    
+    // MARK: - Life Cycle
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.captureSession.sessionPreset = AVCaptureSessionPresetLow
+        for device in AVCaptureDevice.devices()
+        {
+            if device.hasMediaType(AVMediaTypeVideo) && device.position == .Back
+            {
+                self.captureDevice = device as? AVCaptureDevice
+                return
+            }
+        }
     }
-
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.startSession()
+    }
+    
+    override func viewDidDisappear(animated: Bool)
+    {
+        super.viewDidDisappear(animated)
+        self.stopSession()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        self.stopSession()
+    }
+    
+    
+    // MARK: - AV Life Cycle
+    private func startSession()
+    {
+        var error: NSError?
+        let output = AVCaptureMetadataOutput()
+        
+        if let
+            input = AVCaptureDeviceInput(device: self.captureDevice, error: &error)
+        where
+            error == nil &&
+            self.captureSession.canAddInput (input) &&
+            self.captureSession.canAddOutput(output)
+        {
+            self.captureSession.addInput (input)
+            self.captureSession.addOutput(output)
+            
+            let dispatchQueue = dispatch_queue_create("com.stidard.scanner-output-queue", nil)
+            output.setMetadataObjectsDelegate(self, queue: dispatchQueue)
+            output.metadataObjectTypes = [AVMetadataObjectTypeQRCode]
+            
+            self.videoPreviewLayer = AVCaptureVideoPreviewLayer(session: self.captureSession)
+            self.videoPreviewLayer?.videoGravity = AVLayerVideoGravityResizeAspectFill
+            self.videoPreviewLayer?.frame = self.previewView.layer.bounds
+            self.previewView.layer.addSublayer(self.videoPreviewLayer)
+            
+            self.captureSession.startRunning()
+        }
+        else
+        {
+            NSLog("Unable to add Input to capture session: \n\(self.captureSession)\n with device: \n\(self.captureDevice)")
+        }
+    }
+    
+    private func stopSession()
+    {
+        self.captureSession.stopRunning()
+        self.videoPreviewLayer?.removeFromSuperlayer()
+        self._captureSession = nil
+    }
+
+    
+    // MARK: AV QRCode Output
+    func captureOutput(
+        captureOutput: AVCaptureOutput!,
+        didOutputMetadataObjects metadataObjects: [AnyObject]!,
+        fromConnection connection: AVCaptureConnection!)
+    {
+        // NOTE: Callback still in backgroud queue
+        
     }
     
 
