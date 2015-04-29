73a6ae6d441c0d266665c64abf1387cd133a9f5d
James Stidard <jamesstidard@James-MacBook-Pro.local>
2015-04-29 01:52:40 +0100

    bug fix: collection view refresh after new identity added

--- stash/stash/IdentityCollectionViewController.swift
+++ stash/stash/IdentityCollectionViewController.swift
@@ -183,35 +183,6 @@ class IdentityCollectionViewController: UICollectionViewController,
         }
     }
 
-    /*
-    // Uncomment this method to specify if the specified item should be highlighted during tracking
-    override func collectionView(collectionView: UICollectionView, shouldHighlightItemAtIndexPath indexPath: NSIndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment this method to specify if the specified item should be selected
-    override func collectionView(collectionView: UICollectionView, shouldSelectItemAtIndexPath indexPath: NSIndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-    override func collectionView(collectionView: UICollectionView, shouldShowMenuForItemAtIndexPath indexPath: NSIndexPath) -> Bool {
-        return false
-    }
-
-    override func collectionView(collectionView: UICollectionView, canPerformAction action: Selector, forItemAtIndexPath indexPath: NSIndexPath, withSender sender: AnyObject?) -> Bool {
-        return false
-    }
-
-    override func collectionView(collectionView: UICollectionView, performAction action: Selector, forItemAtIndexPath indexPath: NSIndexPath, withSender sender: AnyObject?) {
-    
-    }
-    */
-
     // MARK: - Identity Cell Delegate
     func identityCell(identityCell: IdentityCell, didDecryptStore sensitiveData: NSData)
     {
@@ -236,5 +207,9 @@ class IdentityCollectionViewController: UICollectionViewController,
         }
         
         return nil
+    }
+    
+    func controllerDidChangeContent(controller: NSFetchedResultsController) {
+        self.collectionView?.reloadData()
     }
 }
